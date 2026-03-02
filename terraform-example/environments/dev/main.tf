75e30912d86a07ed406b67dbfab39ff11c8a7735
Ram <metla.ramdas@gmail.com>
2026-03-01 22:51:38 -0500

    fix: remove address_prefixes from data source

--- terraform-example/environments/dev/main.tf
+++ terraform-example/environments/dev/main.tf
@@ -36,7 +36,6 @@ data "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = data.azurerm_resource_group.rg.name
-  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Call the virtual-machine module
