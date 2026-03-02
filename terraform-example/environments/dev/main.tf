30ad8b1c3a34bf1beae2b1a1ae5377f1160e5c97
Ram <metla.ramdas@gmail.com>
2026-03-01 23:02:30 -0500

    fix: use variable names directly for subnet data source

--- terraform-example/environments/dev/main.tf
+++ terraform-example/environments/dev/main.tf
@@ -34,8 +34,8 @@ data "azurerm_virtual_network" "vnet" {
 # Reference existing Subnet
 data "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.resource_group_name
 }
 
 # Reference existing Public IP
